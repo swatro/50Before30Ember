38a206aed1fad49ef79214b9870f2b3e03df7190
Stacey Watro <Staceywatr@gmail.com>
2016-06-18 16:30:47 -0400

    update styling for home page

--- app/controllers/state.js
+++ app/controllers/state.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['name', 'abv'],
   name: null,
-  abv:null,
+  abv: null,
   state: Ember.computed('name', 'abv', function() {
     var name = this.get('name');
     var abv = this.get('abv');
